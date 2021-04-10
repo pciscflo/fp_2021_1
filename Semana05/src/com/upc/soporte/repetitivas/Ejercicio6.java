6a866d0ef8db4e72c7e8f5eb9173381b01864599
Carlos Alberto <pciscflo@upc.edu.pe>
2021-04-10 09:54:55 -0500

    10.04.03

---
+++ Semana05/src/com/upc/soporte/repetitivas/Ejercicio6.java
@@ -0,0 +1,21 @@
+package com.upc.soporte.repetitivas;
+
+public class Ejercicio6 {
+/*
+ Una empresa tiene como reglamento dar aumento de sueldo a sus trabajadores
+ todos los años, el porcentaje de aumento está dado de acuerdo con el tipo
+ de trabajador: Gerente (g) o empleado (e). Los gerentes reciben un aumento
+ del 14% anual y los empleados reciben el 8% anual. Cada 4 años en vez de 14%
+ reciben 18% y en vez de 8% reciben 12% (dependiendo del tipo de trabajador).
+ Desarrollar los módulos que determinen el sueldo que tendrá un trabajador
+ después de N años y el porcentaje de aumento de sueldo que ha obtenido
+ comparando su sueldo original y su sueldo después de N años. Tenga en cuenta
+ que los aumentos obtenidos van a su sueldo.
+ Se le solicita lo siguiente:
+   a.	Cálculo del sueldo después de N años
+   b.	Calcular el porcentaje de aumento después de N años.
+ */
+    public static void main(String[] args) {
+        
+    }
+}
