02eb1afb5a47b599b993c242970680921aa711b0
Carlos Alberto <pciscflo@upc.edu.pe>
2021-05-01 09:38:36 -0500

    01.05.02

---
+++ Semana08/src/com/upc/fundamentos/Ejercicio1.java
@@ -0,0 +1,15 @@
+package com.upc.fundamentos;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        int cuadrado=0;
+        cuadrado = (int)Math.pow(2,3);
+        //asignar un real a entero no es posible en Java
+        int precio = 99999999;
+        long suma =999999999;
+        suma = (int) 999999998.8;
+        System.out.println(suma);
+
+
+    }
+}
